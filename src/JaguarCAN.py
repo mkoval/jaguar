2a7871ed6aa7127e1b8624dd6b229be30181641d
Michael Koval <koval.michael@gmail.com>
2012-02-09 00:28:06 -0500

    Partial PID control support.

--- src/JaguarCAN.py
+++ src/JaguarCAN.py
@@ -1,10 +1,13 @@
 import bitstring, serial, struct
 from collections import namedtuple, OrderedDict
+from datetime import datetime, timedelta
 
 SerialPayload = namedtuple('SerialPayload', [ 'device_id', 'payload' ])
 GenericCANMsg = namedtuple('GenericCANMsg', [ 'device_type', 'manufacturer',
                                               'api_class', 'api_key',
                                               'device_number', 'payload' ])
+MotorCtrl     = namedtuple('MotorCtrl',     [ 'device_type', 'manufacturer',
+                                              'device_number' ])
 
 class Packetizer:
     def __init__(self, sof, esc, sof_esc, esc_esc):
@@ -122,14 +125,240 @@ class JaguarUART:
         framed = self.packetizer.frame_bytes(packed)
         self.serial.write(framed)
 
-    def recv_message(self):
+    def recv_message(self, timeout=None):
+        self.timeout = timeout
         while True:
+            # This may timeout and return zero bytes, so timeout here as well.
             byte = self.serial.read(1)
-            packet = self.packetizer.recv_byte(byte)
+            if byte:
+                return None
 
-            if packet != None:
-                msg = self.parse_message(packet)
-                return packet
+            # Feed the packetizing state machine the next character.
+            packet = self.packetizer.recv_byte(byte)
+            if packet:
+                return self.parse_message(packet)
+
+    def pending_data(self):
+        return self.serial.inWaiting() > 0
+
+class JaguarCAN:
+    class DeviceType:
+        BroadcastMessages   = 0
+        RobotController     = 1
+        MotorController     = 2
+        RelayController     = 3
+        GyroSensor          = 4
+        AccelerometerSensor = 5
+        UltrasonicSensor    = 6
+        GearToothSensor     = 7
+        FirmwareUpdate      = 31
+
+    class Manufacturer:
+        BroadcastMessages   = 0
+        NationalInstruments = 1
+        TexasInstruments    = 2
+        DEKA                = 3
+
+    class APIClass:
+        VoltageControl      = 0
+        SpeedControl        = 1
+        VoltageCompControl  = 2
+        PositionControl     = 3
+        CurrentControl      = 4
+        Status              = 5
+        Configuration       = 7
+        Acknowledge         = 8
+
+    class SystemControl:
+        SystemHalt          = 0
+        SystemReset         = 1
+        DeviceAssignment    = 2
+        DeviceQuery         = 3
+        Heartbeat           = 5
+        SynchronousUpdate   = 6
+        FirmwareUpdate      = 7
+        FirmwareVersion     = 8
+        Enumeration         = 9
+        SystemResume        = 10
+
+    class SpeedControl:
+        SpeedModeEnable     = 0
+        SpeedModeDisable    = 1
+        SpeedSet            = 2
+        SpeedPConstant      = 3
+        SpeedIConstant      = 4
+        SpeedDConstant      = 5
+        SpeedReference      = 6
+
+    class MotorControlStatus:
+        OutputVoltagePerc   = 0
+        BusVoltage          = 1
+        Current             = 2
+        Temperature         = 3
+        Position            = 4
+        Speed               = 5
+        Limit               = 6
+        Fault               = 7
+        Power               = 8
+        ControlMode         = 9
+        OutputVoltageVolts  = 10
+
+    class MotorControlConfiguration:
+        NumBrushes          = 0
+        NumEncoderLines     = 1
+        NumPotTurns         = 2
+        BreakCoastSetting   = 3
+        LimitMode           = 4
+        ForwardDirLimit     = 5
+        ReverseDirLimit     = 6
+        MaximumOutputVolt   = 7
+        FaultTime           = 8
+
+    class SpeedReference:
+        Positive   = 0
+        Negative   = 2
+        Quadrature = 3
+
+    enumerate_timeout = timeval(milliseconds=80)
+
+    def __init__(self, bridge):
+        self.bridge = bridge
+
+    def halt(self):
+        '''
+        Stop driving all motors and go into a neutral state. No motors can not
+        be driven again until either a resume has been issued.
+        '''
+        self.bridge.send_message(GenericCANMsg(
+            device_type   = self.DeviceType.BroadcastMessages,
+            manufacturer  = self.Manufacturer.BroadcastMessages,
+            api_class     = 0, # arbitrary
+            api_key       = self.SystemControl.Halt,
+            device_number = 0, # arbitrary
+            payload       = b''
+        ))
+
+    def resume(self):
+        '''
+        Enable motor control. See halt() for more information.
+        '''
+        self.bridge.send_message(GenericCANMsg(
+            device_type   = self.DeviceType.BroadcastMessages,
+            manufacturer  = self.Manufacturer.BroadcastMessages,
+            api_class     = 0, # arbitrary
+            api_key       = self.SystemControl.Resume,
+            device_number = 0, # arbitrary
+            payload       = b''
+        ))
+
+    def update(self, mask):
+        '''
+        Trigger a synchronous update all motor controllers with a group that
+        matches the bitmask.
+        '''
+        self.bridge.send_message(GenericCANMsg(
+            device_type   = self.DeviceType.BroadcastMessages,
+            manufacturer  = self.Manufacturer.BroadcastMessages,
+            api_class     = 0, # arbitrary
+            api_key       = self.SystemControl.SynchronousUpdate,
+            device_number = 0, # arbitrary
+            payload       = struct.pack('B', mask)
+        ))
+
+    def heartbeat(self):
+        '''
+        Reset the watchdogs on all connected motor controllers. Every motor
+        controller must receive a minimum of one CAN message every 100 ms to
+        avoid entering a fault state.
+        '''
+        self.bridge.send_message(GenericCANMsg(
+            device_type   = self.DeviceType.BroadcastMessages,
+            manufacturer  = self.Manufacturer.BroadcastMessages,
+            api_class     = 0, # arbitrary
+            api_key       = self.SystemControl.Heartbeat,
+            device_number = 0, # arbitrary
+            payload       = b''
+        ))
+
+    def enumerate(self):
+        '''
+        Obtain a list of motor controllers connected to this CAN bus bridge by
+        requesting each controller to enumerate itself. This enumeration takes
+        a minimum of 80 ms.
+        '''
+        self.bridge.send_message(GenericCANMsg(
+            device_type   = self.DeviceType.BroadcastMessages,
+            manufacturer  = self.Manufacturer.BroadcastMessages,
+            api_class     = 0, # arbitrary
+            api_key       = self.SystemControl.Enumeration,
+            device_number = 0, # arbitrary
+            payload       = b''
+        ))
+
+        controllers = list()
+        end = datetime.now() + self.enumerate_timeout
+        while datetime.now() < end:
+            remaining = (end - datetime.now()).total_seconds()
+            msg = self.bridge.recv_message(timeout=remaining)
+
+            if msg:
+                ctrl = MotorCtrl(
+                    device_type=msg.device_type,
+                    manufacturer=msg.manufacturer,
+                    device_number=msg.device_number
+                )
+                controllers.append(ctrl)
+
+        return ids
+
+class Jaguar:
+    def __init__(self, bridge, device_number):
+        self.bridge = bridge
+        self.number = device_number
+
+        # Use a quadrature encoder to measure velocity.
+        payload = struct.pack('B', JaguarCAN.SpeedReference.Quadrature)
+        self._send_message(JaguarCAN.SpeedControl.SpeedReference, payload)
+
+    def enable(self):
+        '''
+        Enables speed control and sets the output to neutral.
+        '''
+        self._send_message(JaguarCAN.SpeedControl.SpeedModeEnable, b'')
+
+    def disable(self):
+        '''
+        Disables speed control, returns to the default control mode, and sets
+        the output to neutral.
+        '''
+        self._send_message(JaguarCAN.SpeedControl.SpeedModeDisable, b'')
+
+    def set_speed(self, speed, group=0):
+        '''
+        Sets the target rotational speed in revolutions per minute.
+        '''
+        payload_speed = struct.pack('<i', ???)
+        payload_group = struct.pack('B', group)
+        payload       = payload_speed + payload_group
+        self._send_message(self.SpeedControl.SpeedSet, payload)
+
+    def set_constants(self, kp, ki, kd):
+        payload_kp = struct.pack('<i', ???)
+        payload_ki = struct.pack('<i', ???)
+        payload_kd = struct.pack('<i', ???)
+        self._send_message(self.SpeedControl.SetPConstant, payload_kp)
+        self._send_message(self.SpeedControl.SetIConstant, payload_ki)
+        self._send_message(self.SpeedControl.SetDConstant, payload_kd)
+
+    def _send_message(self, api_key, payload):
+        self.bridge.send_message(GenericCANMsg(
+            device_type   = self.DeviceType.MotorController,
+            manufacturer  = self.Manufacturer.TexasInstruments,
+            api_class     = self.APIClass.SpeedControl,
+            api_key       = api_key,
+            device_number = self.number,
+            payload       = payload
+        ))
 
 def main():
     serial = serial.Serial(
