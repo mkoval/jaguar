8ce7cbf5b3442df9e8d98a039424537c222c8038
Cody Schafer <jmesmon@gmail.com>
2012-03-20 02:09:39 -0500

    Add initial "unbrick"
    Restructure non-ros makefile (still named Makefile.osx)
    Add timed_block() to TokenPtr.
    Add attach_callback(id, id_mask, callback).
    Add FirmwareUpdate api enum.
    Add pack_id with a unified "api" field.

--- include/jaguar/jaguar_bridge.h
+++ include/jaguar/jaguar_bridge.h
@@ -26,6 +26,38 @@ enum ReceiveState {
     kComplete
 };
 
+template<typename T>
+class masked_number
+{
+public:
+	typedef T type;
+
+	masked_number(T const& num_, T const& mask_)
+	: num(num_), mask(mask_)
+	{
+		assert((num_ & ~mask_) == 0);
+	}
+
+	template <class U>
+	masked_number(masked_number<U> const &mn)
+	: num(mn.num), mask(mn.mask)
+	{}
+
+	bool matches(T val)
+	{
+		return (val & mask) == num;
+	}
+
+	T num;
+	T mask;
+};
+
+template<typename T>
+masked_number<T> make_masked_number(T const &n, T const &m)
+{
+	return masked_number<T>(n, m);
+}
+
 class JaguarToken;
 
 class JaguarBridge : public CANBridge
@@ -38,12 +70,18 @@ public:
     virtual TokenPtr recv(uint32_t id);
 
     virtual void attach_callback(uint32_t id, recv_callback cb);
+    virtual void attach_callback(uint32_t id, uint32_t id_mask,
+		    recv_callback cb);
     //virtual bool detach_callback(uint32_t id, recv_callback cb);
 
 private:
     typedef boost::signal<void (boost::shared_ptr<CANMessage>)> callback_signal;
     typedef boost::shared_ptr<callback_signal> callback_signal_ptr;
+
     typedef std::map<uint32_t, callback_signal_ptr> callback_table;
+
+    typedef std::pair<masked_number<uint32_t>, callback_signal_ptr> mask_callback;
+    typedef std::list<mask_callback> callback_list;
 
     typedef boost::shared_ptr<JaguarToken> token_ptr;
     typedef std::map<uint32_t, token_ptr>  token_table;
@@ -58,6 +96,7 @@ private:
     boost::thread recv_thread_;
     std::vector<uint8_t> recv_buffer_;
     callback_table callbacks_;
+    callback_list  callbacks_list_;
     boost::mutex callback_mutex_;
     token_table tokens_;
 
@@ -78,6 +117,7 @@ class JaguarToken : public Token {
 public:
     virtual ~JaguarToken(void);
     virtual void block(void);
+    virtual bool timed_block(boost::posix_time::time_duration const &duration);
     virtual boost::shared_ptr<CANMessage const> message(void) const;
     virtual bool ready(void) const;
 
@@ -96,3 +136,5 @@ private:
 };
 
 #endif
+
+/* vim set: sts=4 et sw=4 ts=4 : */
--- src/jaguar_helper.cc
+++ src/jaguar_helper.cc
@@ -23,16 +23,30 @@ double s16p16_to_double(int32_t x)
     return x / 65536.;
 }
 
-uint32_t pack_id(uint8_t num, Manufacturer::Enum man, DeviceType::Enum type,
+
+uint32_t pack_id(uint8_t dnum, Manufacturer::Enum man, DeviceType::Enum type, uint16_t api)
+{
+    assert((dnum & ~0x3F) == 0);
+    assert((api  &  ~0x3FF) == 0);
+    assert((man  & ~0xFF) == 0);
+    assert((type & ~0x1F) == 0);
+    return (static_cast<uint32_t>(dnum) << 0)
+         | (static_cast<uint32_t>(api)  << 6)
+         | (static_cast<uint32_t>(man)  << 16)
+         | (static_cast<uint32_t>(type) << 24);
+}
+
+
+uint32_t pack_id(uint8_t dnum, Manufacturer::Enum man, DeviceType::Enum type,
 	                     APIClass::Enum api_class, uint8_t api_index)
 {
-    assert((num       & ~0x3F) == 0);
+    assert((dnum      & ~0x3F) == 0);
     assert((api_class & ~0x3F) == 0);
     assert((api_index & ~0x0F) == 0);
     assert((man       & ~0xFF) == 0);
     assert((type      & ~0x1F) == 0);
 
-    return (static_cast<uint32_t>(num)       << 0)
+    return (static_cast<uint32_t>(dnum)      << 0)
          | (static_cast<uint32_t>(api_index) << 6)
          | (static_cast<uint32_t>(api_class) << 10)
          | (static_cast<uint32_t>(man)       << 16)
@@ -45,3 +59,5 @@ uint32_t pack_ack(uint8_t num, Manufacturer::Enum man, DeviceType::Enum type)
 }
 
 };
+
+/* vim: set ts=4 et sts=4 sw=4: */
