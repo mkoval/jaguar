55d3239928647a5b7412893975d03a4741e4530e
Cody Schafer <jmesmon@gmail.com>
2012-03-21 00:04:23 -0500

    unbrick:preliminary flashing.

--- src/unbrick.cc
+++ src/unbrick.cc
@@ -1,6 +1,11 @@
+#include <stdint.h>
 #include <iostream>
 #include <fstream>
+#include <boost/assert.hpp>
 #include <boost/lambda/lambda.hpp>
+#include <boost/fusion/algorithm.hpp>
+#include <boost/spirit/include/karma.hpp>
+#include <boost/foreach.hpp>
 #include <jaguar/jaguar_bridge.h>
 #include <jaguar/jaguar_helper.h>
 
@@ -28,29 +33,44 @@ static void upd_send(can::CANBridge &can, uint16_t api)
     can.send(can::CANMessage(upd_id(api)));
 }
 
-__attribute__((__unused__))
 static void upd_send(can::CANBridge &can, uint16_t api,
     std::vector<uint8_t> const &payload)
 {
     can.send(can::CANMessage(upd_id(api), payload));
 }
 
+template <typename generator_type>
+static void f_send(can::CANBridge &can, uint16_t api,
+        generator_type generator)
+{
+    std::vector<uint8_t> obuf;
+    BOOST_VERIFY(boost::spirit::karma::generate(obuf.end(), generator));
+    upd_send(can, api, obuf);
+}
+
 int main(int argc, char *argv[])
 {
     try {
-        if (argc <= 2) {
+        if (argc <= 3) {
             char const *n = argc?argv[0]:"./unbrick";
             std::cerr << "err: lack args\n"
-                      << "usage: " << n << " <serial> <fw.bin>" << std::endl;
+                      << "usage: " << n << " <serial> <fw.bin> <start addr>" << std::endl;
             return 1;
         }
 
         std::string const io_path(argv[1]);
         std::string const fw_path(argv[2]);
+        std::stringstream sa_stm;
+        uint32_t fw_start;
+        sa_stm << argv[3];
+        sa_stm >> fw_start;
 
         can::JaguarBridge can(io_path);
         /* XXX: the hell is this, C++? */
-        std::ifstream     fw(fw_path.c_str(), std::ifstream::in);
+        std::ifstream     fw_stream(fw_path.c_str());
+        std::stringstream fw_buf;
+        fw_buf << fw_stream.rdbuf();
+        std::string const fw(fw_buf.str());
 
         can.attach_callback(0, 0, std::cerr << boost::lambda::_1);
 
@@ -62,11 +82,29 @@ int main(int argc, char *argv[])
             std::cout << "p" << std::endl;
         } while(!ping_token->timed_block(boost::posix_time::millisec(50)));
 
-        /* TODO: set starting address */
-        upd_send(can, jaguar::FirmwareUpdate::kDownload);
-        /* TODO: ack? */
+        /* set starting address and length */
+        f_send(can, jaguar::FirmwareUpdate::kDownload,
+                boost::spirit::karma::little_dword(fw_start) <<
+                boost::spirit::karma::little_dword(fw.size()));
+        /* TODO: wait for ack? */
 
-        /* TODO: send data block */
+        /* send data block */
+        std::vector<uint8_t> data;
+        BOOST_FOREACH(uint8_t byte, fw) {
+            if (data.size() == 8) {
+                /* send `data` and reset */
+                upd_send(can, jaguar::FirmwareUpdate::kSendData, data);
+                /* TODO: wait for ack? */
+
+                data.clear();
+            }
+
+            data.push_back(byte);
+        }
+
+        if (!data.empty()) {
+            upd_send(can, jaguar::FirmwareUpdate::kSendData, data);
+        }
 
     } catch (can::CANException &e) {
         std::cerr << "error " << e.code() << ": " << e.what() << std::endl;
