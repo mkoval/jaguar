40dee4f63c408322d5756255c5f5e77253b0460b
Michael Koval <koval.michael@gmail.com>
2012-03-22 16:37:05 -0400

    Use a mutex to control the serial port.

--- src/diff_drive.cc
+++ src/diff_drive.cc
@@ -18,18 +18,17 @@ DiffDriveRobot::DiffDriveRobot(DiffDriveSettings const &settings)
       timer_(boost::bind(&DiffDriveRobot::heartbeat, this)),
       robot_radius_(settings.robot_radius_m)
 {
-    // Configure the number of "lines" on each encoder. 
     double const ticks_per_meter = settings.ticks_per_rev / (2. * M_PI * settings.wheel_radius_m);
-    block(
-        jag_left_.config_encoders_set(ticks_per_meter),
-        jag_right_.config_encoders_set(ticks_per_meter)
-    );
-
     speed_init();
     odom_init();
 
     // This is necessary for the Jaguars to work after a fresh boot, even if
     // we never called system_halt() or system_reset().
+    boost::mutex::scoped_lock lock(mutex_);
+    block(
+        jag_left_.config_encoders_set(ticks_per_meter),
+        jag_right_.config_encoders_set(ticks_per_meter)
+    );
     jag_broadcast_.system_resume();
 }
 
@@ -48,6 +47,7 @@ void DiffDriveRobot::drive(double v, double omega)
 
 void DiffDriveRobot::drive_raw(double v_left, double v_right)
 {
+    boost::mutex::scoped_lock lock(mutex_);
     block(
         jag_left_.speed_set(v_left),
         jag_right_.speed_set(v_right)
@@ -73,6 +73,7 @@ void DiffDriveRobot::odom_init(void)
     // speed reference for velocity control and position reference for
     // odometry. As such, they must be configured for position control even
     // though we are are using speed control mode.
+    boost::mutex::scoped_lock lock(mutex_);
     block(
         jag_left_.position_set_reference(PositionReference::kQuadratureEncoder),
         jag_right_.position_set_reference(PositionReference::kQuadratureEncoder)
@@ -141,25 +142,31 @@ void DiffDriveRobot::odom_update(Side side, int32_t &last_pos, int32_t &curr_pos
  */
 void DiffDriveRobot::speed_set_p(double p)
 {
+    boost::mutex::scoped_lock lock(mutex_);
     block(jag_left_.speed_set_p(p), jag_right_.speed_set_p(p));
 }
 
 void DiffDriveRobot::speed_set_i(double i)
 {
+    boost::mutex::scoped_lock lock(mutex_);
     block(jag_left_.speed_set_p(i), jag_right_.speed_set_p(i));
 }
 
 void DiffDriveRobot::speed_set_d(double d)
 {
+    boost::mutex::scoped_lock lock(mutex_);
     block(jag_left_.speed_set_p(d), jag_right_.speed_set_p(d));
 }
 
 void DiffDriveRobot::speed_init(void)
 {
-    block(
-        jag_left_.speed_set_reference(SpeedReference::kQuadratureEncoder),
-        jag_right_.speed_set_reference(SpeedReference::kQuadratureEncoder)
-    );
+    {
+        boost::mutex::scoped_lock lock(mutex_);
+        block(
+            jag_left_.speed_set_reference(SpeedReference::kQuadratureEncoder),
+            jag_right_.speed_set_reference(SpeedReference::kQuadratureEncoder)
+        );
+    }
     speed_set_p(1000.0);
     speed_set_i(0.0);
     speed_set_d(0.0);
@@ -171,7 +178,10 @@ void DiffDriveRobot::speed_init(void)
 void DiffDriveRobot::heartbeat(void)
 {
     for (;;) {
-        jag_broadcast_.heartbeat();
+        {
+            boost::mutex::scoped_lock lock(mutex_);
+            jag_broadcast_.heartbeat();
+        }
 
         // We must gracefully handle interruption because the destructor
         // interrupts this thread to cleanly exit.
